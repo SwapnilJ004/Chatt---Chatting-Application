a9a38a3da8ef923e45a32ba5e87d055cfb0f5382
swapnil0154 <joshiswapnil2004@gmail.com>
2024-07-22 22:58:43 +0530

    Get Message Api Route

--- BackEnd/controller/message.controller.js
+++ BackEnd/controller/message.controller.js
@@ -31,7 +31,31 @@ export const sendMessage = async(req, res, next) => {
         await Promise.all([conversation.save(), newMessage.save()])
 
         //Socket IO functionality
+
+
         res.status(201).json(newMessage)
+    }
+    catch(error){
+        next(error)
+    }
+}
+
+export const receiveMessage = async(req, res, next) => {
+    try{
+        const {id: userToMessage} = req.params
+        const senderId = req.user.id
+
+        const conversation = await Conversation.findOne({
+            participants: {$all: [senderId, userToMessage]}
+        }).populate("messages")
+        
+        if(!conversation){
+            return res.status(200).json([])
+        }
+
+        const messages = conversation.messages
+
+        res.status(200).json(messages)
     }
     catch(error){
         next(error)
--- BackEnd/routes/message.routes.js
+++ BackEnd/routes/message.routes.js
@@ -1,8 +1,11 @@
 import express from "express"
 import { isAuthenticated } from "../Middleware/verifyToken.js"
 import { sendMessage } from "../controller/message.controller.js"
+import { receiveMessage } from "../controller/message.controller.js"
 
 const router = express.Router()
 
 router.post("/send/:id", isAuthenticated, sendMessage)
+
+router.get("/:id", isAuthenticated, receiveMessage)
 export default router;
