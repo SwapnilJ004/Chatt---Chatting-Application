27268617065d0507a7abb182ac00cd690469c871
swapnil0154 <joshiswapnil2004@gmail.com>
2024-08-02 01:10:16 +0530

    Login Page UI

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom"; 
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
+import Home from './pages/Home.jsx'
 
 const App = () => {
-  return <div>
-  <button className="btn">Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-ghost">Ghost</button>
-<button className="btn btn-link">Link</button>
+  return (
+  <div className="p-4 h-screen flex items-center justfiy-center">
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<Signup />} />
+  </Routes>
+ 
   </div>
-}
+  )
+  }
 
 export default App
